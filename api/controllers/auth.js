d73ef216efdcd3eb8d741a1fc3b976660b14a0c7
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-10-21 13:10:54 +0530

    modified

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -6,23 +6,24 @@ import jwt from "jsonwebtoken";
 export const register = (req, res) => {
     //CHECK EXISTING USER
     const q = "SELECT * FROM users WHERE email = ? OR username = ?";
-
-    db.query(q, (err, data) => {
+  
+    db.query(q, [req.body.email, req.body.username], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.length) return res.status(409).json("User already exists!");
+  
+      //Hash the password and create a user
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+  
+      const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+      const values = [req.body.username, req.body.email, hash];
+  
+      db.query(q, [values], (err, data) => {
         if (err) return res.status(500).json(err);
-        if (data.length) return res.status(409).json("User already exists!");
-
-        //Hash the password and create a user
-        const salt = bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt);
-
-        const q = "INSERT INTO users(`username`,`email`,`password`) VALUES ('" + req.body.username + "','" + req.body.email + "','" + hash + "')";
-
-        db.query(q, (err, data) => {
-            if (err) return res.status(500).json(err);
-            return res.status(200).json("User has been created.");
-        });
+        return res.status(200).json("User has been created.");
+      });
     });
-};
+  };
 
 export const login = async (req, res) => {
     //CHECK USER
@@ -37,9 +38,9 @@ export const login = async (req, res) => {
         return res.status(400).json('Please verify that you are not a robot');
     }
 
-    const q = "SELECT * FROM users WHERE username = '" + req.body.username + "'";
+    const q = "SELECT * FROM users WHERE username = ?";
 
-    db.query(q, (err, data) => {
+  db.query(q, [req.body.username], (err, data) => {
         if (err) return res.status(500).json(err);
         if (data.length === 0) return res.status(404).json("User not found!");
 
--- api/controllers/post.js
+++ api/controllers/post.js
@@ -3,15 +3,15 @@ import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
     const q = req.query.cat
-        ? "SELECT * FROM posts WHERE cat='" + req.query.cat + "'"
-        : "SELECT * FROM posts";
-
-    db.query(q, (err, data) => {
-        if (err) return res.status(500).send(err);
-
-        return res.status(200).json(data);
+      ? "SELECT * FROM posts WHERE cat=?"
+      : "SELECT * FROM posts";
+  
+    db.query(q, [req.query.cat], (err, data) => {
+      if (err) return res.status(500).send(err);
+  
+      return res.status(200).json(data);
     });
-};
+  };
 
 export const getPost = (req, res) => {
     const q =
